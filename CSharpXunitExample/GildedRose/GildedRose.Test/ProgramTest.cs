16eaebe52e3920d4ba291431e1cecfbd8b301b21
Kevin Buchanan <kevaustinbuch@gmail.com>
2016-07-27 10:38:12 -0500

    Add GildedRose

---
+++ CSharpXunitExample/GildedRose/GildedRose.Test/ProgramTest.cs
@@ -0,0 +1,11 @@
+﻿using Xunit;
+
+namespace GildedRose.Test {
+    public class ProgramTest {
+        [Fact]
+        public void ItemHasName() {
+            var item = new Item {Name = "Bread"};
+            Assert.Equal("Bread", item.Name);
+        }
+    }
+}
---
+++ CSharpXunitExample/GildedRose/GildedRose/Item.cs
@@ -0,0 +1,7 @@
+﻿namespace GildedRose {
+    public class Item {
+        public string Name { get; set; }
+        public int SellIn { get; set; }
+        public int Quality { get; set; }
+    }
+}
